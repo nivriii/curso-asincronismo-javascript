838755668dc08e1fec1e36d258525590c5402b85
nivriii <nivri.learning@gmial.com>
2021-04-12 22:53:00 -0500

    Múltiples Peticiones a un API con Callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,14 @@
+/*
+    - Hacer una petición a nuestra API
+    - Obtener cuantos elementos tiene de personajes
+
+    - Acceder a el primer personaje 
+    - Obtener la ubicación donde se encuentra
+    - Saber en que dimensión se encuentra
+*/
+
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
@@ -11,10 +21,24 @@ function fetchData(url_api, callback) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error ' + url_api);
-                return callback(Error, null)
+                return callback(error, null)
             }
         }
     }
 
     xhttp.send();
 }
+
+// Se generará un Callback hell
+fetchData(API, function(error1, data1) {
+    if (error1) return console.error(erro1);
+    fetchData(API + data1.results[0].id, function(error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
